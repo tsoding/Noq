93c7ac5d0ca7352d0b62029d90aaa8cf741a91c9
rexim <reximkut@gmail.com>
2022-04-16 05:23:51 +0700

    Add more TODOs

--- src/main.rs
+++ src/main.rs
@@ -1052,4 +1052,6 @@ fn main() {
     }
 }
 
+// TODO: `undo` command should remove previous command from the history
+// TODO: Ability to restore saved session
 // TODO: Custom arbitrary operators like in Haskell
